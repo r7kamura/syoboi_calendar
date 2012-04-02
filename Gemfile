7b4eea92948f9d9e0c1cee809b19b4eb5a08778a
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-03 01:40:40 +0900

    Initial commit
    has these functions
    * Login
    * Simple Search

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "http://rubygems.org"
+
+gem "mechanize"
+gem "slop"
+
+group :development do
+  gem "pry"
+  gem "awesome_print"
+end
---
+++ main.rb
@@ -0,0 +1,56 @@
+require "bundler/setup"
+Bundler.require(:default, :development)
+
+module ShoboiCalendar
+  class ApiClient
+    BASE_URL   = "http://cal.syoboi.jp"
+    JSON_URL   = BASE_URL + "/json.php"
+    LOGIN_URL  = BASE_URL + "/usr"
+    SEARCH_URL = BASE_URL + "/find"
+
+    def initialize(args)
+      @user     = args[:user]
+      @pass     = args[:pass]
+      @agent    = Mechanize.new
+      @is_login = false
+    end
+
+    def login
+      return if @is_login
+      page = @agent.get(LOGIN_URL)
+      form = page.forms[1]
+      form.usr  = @user
+      form.pass = @pass
+      @agent.submit(form)
+      @is_login = true
+    end
+
+    def search
+      url = create_search_url
+      @agent.get(url)
+    end
+
+    private
+
+    def create_search_url
+      SEARCH_URL + "?" + {
+        :sd  => 2, # program
+        :uud => 1, # user setting
+        :v   => 0, # list
+        :r   => 0, # all range
+      }.map { |k, v| "#{k}=#{v}" }.join("&")
+    end
+  end
+end
+
+args = Slop.parse :help => true do
+  on :u, :user=, "Username"
+  on :p, :pass=, "Password"
+end
+
+client = ShoboiCalendar::ApiClient.new(
+  :user => args[:user],
+  :pass => args[:pass],
+)
+client.login
+require "pry"; binding.pry
