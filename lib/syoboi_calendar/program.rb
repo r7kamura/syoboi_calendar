f344e6f329b012572587e0354692d32ce0dcecec
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-04 21:08:15 +0900

    Refactor: change method_missing to define_method
    better in terms of complementation

--- lib/syoboi_calendar/program.rb
+++ lib/syoboi_calendar/program.rb
@@ -1,45 +1,67 @@
 module SyoboiCalendar
   class Program
-    EXT_PROGRAM_PARAM_MAP = {
-      :channel_epd_url => "ChEPGURL",
-      :channel_id      => "ChID",
-      :channel_name    => "ChName",
-      :comment         => "ConfFlag",
-      :config_flag     => "Count",
-      :count           => "EdTime",
-      :end_time        => "ProgComment",
-      :start_time      => "StTime",
-      :subtitle        => "SubTitle2"
-    }
-    EXT_TITLE_PARAM_MAP = {
-      :cat              => "Cat",
-      :comment          => "Comment",
-      :first_channel    => "FirstCh",
-      :first_end_month  => "FirstEndMonth",
-      :first_end_year   => "FirstEndYear",
-      :first_month      => "FirstMonth",
-      :first_year       => "FirstYear",
-      :keywords         => "Keywords",
-      :short_title      => "ShortTitle",
-      :subtitles        => "SubTitles",
-      :title_en         => "TitleEN",
-      :title_flag       => "TitleFlag",
-      :title_view_count => "TitleViewCount",
-      :title_yomi       => "TitleYomi",
-      :user_point       => "UserPoint",
-      :user_point_rank  => "UserPointRank"
-    }
-
     attr_reader(
       :pid,
       :tid,
       :title
     )
 
+    # need to request JSON API as query parameter
+    REQ_MAP = {
+      :program => "ProgramByPID",
+      :title   => "TitleFull"
+    }
+
+    # :method_name => "NameInSyoboiCalendarResponse"
+    EXT_PARAM_MAP = {
+      :program => {
+        :channel_epd_url => "ChEPGURL",
+        :channel_id      => "ChID",
+        :channel_name    => "ChName",
+        :comment         => "ConfFlag",
+        :config_flag     => "Count",
+        :count           => "EdTime",
+        :end_time        => "ProgComment",
+        :start_time      => "StTime",
+        :subtitle        => "SubTitle2"
+      },
+      :title => {
+        :cat              => "Cat",
+        :comment          => "Comment",
+        :first_channel    => "FirstCh",
+        :first_end_month  => "FirstEndMonth",
+        :first_end_year   => "FirstEndYear",
+        :first_month      => "FirstMonth",
+        :first_year       => "FirstYear",
+        :keywords         => "Keywords",
+        :short_title      => "ShortTitle",
+        :subtitles        => "SubTitles",
+        :title_en         => "TitleEN",
+        :title_flag       => "TitleFlag",
+        :title_view_count => "TitleViewCount",
+        :title_yomi       => "TitleYomi",
+        :user_point       => "UserPoint",
+        :user_point_rank  => "UserPointRank"
+      }
+    }
+
+    # Example:
+    #   program.first_channel
+    #   program.start_time
+    #   ...
+    [:program, :title].each do |type|
+      EXT_PARAM_MAP[type].keys.each do |key|
+        define_method(key) do
+          update_detail(type) unless @blob.has_key?(key)
+          @blob[key]
+        end
+      end
+    end
+
     # NOTICE: @blob is empty hash at first
     # For example,
-    #   When self.channel_name is called,
-    #   update_detail is called automatically
+    #   If self.channel_name is called,
+    #   then update_detail is called automatically,
     #   and @blob is updated to fill up @blob[:channel_name]
     def initialize(args)
       @pid   = args[:pid] or return
@@ -48,49 +70,17 @@ module SyoboiCalendar
       @blob  = {}
     end
 
-    # get more detail of the program and update @blob
-    def update_detail(type)
-      case type
-      when :program
-        return if @has_ext_program_params
-        hash = get_json(:Req => "ProgramByPID")["Programs"][@pid.to_s]
-        EXT_PROGRAM_PARAM_MAP.each { |k, v| @blob[k] = hash[v] }
-        @has_ext_program_params = true
-      when :title
-        return if @has_ext_title_params
-        hash = get_json(:Req => "TitleFull")["Titles"][@tid.to_s]
-        EXT_TITLE_PARAM_MAP.each { |k, v| @blob[k] = hash[v] }
-        @has_ext_title_params = true
-      end
-    end
-
     # SAISOKU means "the most earliest broadcasting in the World"
     def is_saisoku?
       channel_name.match(first_channel) or first_channel.match(channel_name)
     end
 
-    private
-
-    # call update_detail if method name matches param name
-    def method_missing(name, *args)
-      if EXT_PROGRAM_PARAM_MAP[name]
-        update_detail(:program)
-        @blob[name]
-      elsif EXT_TITLE_PARAM_MAP[name]
-        update_detail(:title)
-        @blob[name]
-      else
-        super
-      end
-    end
-
-    # adjust query and get parsed json data
-    def get_json(opts)
-      Agent.json({
-        :Req => "ProgramByPID",
-        :PID => @pid,
-        :TID => @tid
-      }.merge(opts))
+    # get more detail of the program and update @blob
+    def update_detail(type)
+      hash = Agent.json(:Req => REQ_MAP[type], :PID => @pid, :TID => @tid)
+      hash = hash[hash.keys.first]
+      hash = hash[hash.keys.first]
+      EXT_PARAM_MAP[type].each { |k, v| @blob[k] = hash[v] }
     end
   end
 end
