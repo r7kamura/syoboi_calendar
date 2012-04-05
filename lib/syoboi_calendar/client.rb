cdab0826a97f42ecd15fb511a379c7230591b19b
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-05 21:47:03 +0900

    Refactor create_search_query

--- lib/syoboi_calendar/client.rb
+++ lib/syoboi_calendar/client.rb
@@ -47,43 +47,22 @@ module SyoboiCalendar
       programs
     end
 
-    # Adjust opts and create URL to search programs or titles
-    #   sd:  program(2) or title(0)
-    #   uuc: narrow the search to login user's channel setting
-    #   v:   return list(0)
-    #   r:   range type(0..3)
-    #   rd:  range(str)
+    # create hash for search query
     def create_search_query(opts)
-      hash = {}
-
-      case opts[:mode]
-      when :title then hash[:sd] = 0
-      else             hash[:sd] = 2
-      end
-
-      case opts[:range]
-      when :all, nil then hash[:r] = 0
-      when :past     then hash[:r] = 1
-      when :future   then hash[:r] = 2
-      else                hash[:r] = 3
-        hash[:rd] = opts[:range]
-      end
-
-      hash[:pfn] = 2 if opts[:first]   # first episode
-      hash[:pfl] = 4 if opts[:final]   # final episode
-      hash[:pfn] = 1 if opts[:special] # special program
-
       {
-        :title    => :kw,
-        :channel  => :ch,
-        :subtitle => :st,
-        :comment  => :cm,
-      }.each { |k, v| hash[v] = opts[k] if opts[k] }
-
-      {
-        :uuc => 1,
-        :v   => 0,
-      }.merge(hash)
+        :sd  => { nil => 2, :program => 2, :title => 0 }[opts[:mode]],
+        :r   => { nil => 0, :all => 0, :past => 1, :future => 2}[opts[:range]] || 3,
+        :rd  => opts[:range],
+        :kw  => opts[:title],
+        :ch  => opts[:channel],
+        :st  => opts[:subtitle],
+        :cm  => opts[:comment],
+        :uuc => opts[:uuc] || 1,     # use user's channel setting
+        :v   => opts[:v] || 0,       # list view
+        :pfn => opts[:first] && 2,   # first episode
+        :pfl => opts[:final] && 4,   # final episode
+        :pfs => opts[:special] && 1, # special program
+      }.select { |k, v| v }
     end
   end
 end
