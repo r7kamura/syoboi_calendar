59ccf767e0992cc64fc51178ccd0295bbd0915d8
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-05 22:51:09 +0900

    Refactor: unnecessary module definition

--- lib/syoboi_calendar.rb
+++ lib/syoboi_calendar.rb
@@ -2,8 +2,6 @@ require "json"
 require "bundler/setup"
 Bundler.require(:default)
 
-module SyoboiCalendar
-  require "syoboi_calendar/agent"
-  require "syoboi_calendar/program"
-  require "syoboi_calendar/client"
-end
+require "syoboi_calendar/agent"
+require "syoboi_calendar/program"
+require "syoboi_calendar/client"
