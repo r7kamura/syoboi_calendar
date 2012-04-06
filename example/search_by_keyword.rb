d6218406c7c52f8e6311b81e24f48b3b2f4240b0
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-06 23:02:47 +0900

    Refactor: range should be displayed earlier

--- example/search_by_keyword.rb
+++ example/search_by_keyword.rb
@@ -6,8 +6,8 @@ require "syoboi_calendar"
 require "slop"
 args = Slop.parse :help => true do
   banner "usage: \n    ruby #{__FILE__}"
-  on :r, :range=, "Date range (ex. 2012/4/1-2012/4/30)"
   on :k, :keyword=, "Keyword for title (ex. ひだまりスケッチ)"
+  on :r, :range=, "Date range (ex. 2012/4/1-2012/4/30)"
 end
 unless args[:keyword]
   puts args.to_s.gsub(/\n\n/, "\n")
