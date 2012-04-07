2632414146c387274257b112c2f5b900a447aa90
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 08:22:15 +0900

    Create SyoboiCalendar::Fixture for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,25 @@ $LOAD_PATH.unshift File.expand_path("../", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "syoboi_calendar"
+require "webmock/rspec"
+WebMock.disable!
+
+module SyoboiCalendar::Fixture
+  extend self
+
+  FIXTURE_DIR = File.expand_path("../fixtures", __FILE__)
+
+  private
+
+  def method_missing(method_name, *args)
+    return if cache[method_name]
+    path = "#{FIXTURE_DIR}/#{method_name}"
+    File.exist?(path) ?
+      cache[method_name] = File.read(path) :
+      nil
+  end
+
+  def cache
+    @cache ||= {}
+  end
+end
