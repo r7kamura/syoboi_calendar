630b4022ad1f96a3c242713d845c3de027800a68
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 08:22:39 +0900

    Add spec for search with WebMock

--- spec/syoboi_calendar/client_spec.rb
+++ spec/syoboi_calendar/client_spec.rb
@@ -1,11 +1,37 @@
 require "spec_helper"
 
 describe "SyoboiCalendar::Client" do
-  before do
+  before(:all) do
+    stub_request(:any, SyoboiCalendar::Agent::SEARCH_URL).\
+      with(:body => SyoboiCalendar::Fixture.response_from_search_programs)
+
     user = "testuserforruby"
     pass = user
     @client = SyoboiCalendar::Client.new(:user => user, :pass => pass)
     @guest  = SyoboiCalendar::Client.new
+  end
+
+  describe "#search" do
+    before(:all) do
+      WebMock.enable!
+    end
+    after(:all) do
+      WebMock.disable!
+    end
+
+    before do
+      @args = { :range => "2012/4/1-2012/4/30" }
+    end
+
+    it do
+      @client.search(@args).should be_kind_of Array
+    end
+
+    context "in default" do
+      it "should return Programs" do
+        @client.search(@args)[0].should be_kind_of SyoboiCalendar::Program
+      end
+    end
   end
 
   describe "#login?" do
