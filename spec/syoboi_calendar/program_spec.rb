d8415afb2b75264f4a9e016e234c347ad37679e8
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 19:26:35 +0900

    Spec for saisoku

---
+++ spec/syoboi_calendar/program_spec.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+require "spec_helper"
+
+describe "SyoboiCalendar::Program" do
+  before do
+    @program = SyoboiCalendar::Program.new(
+      :tid          => 2481,
+      :pid          => 217175,
+      :name         => "聖闘士星矢Ω #1 星矢によって救われた命！甦れ聖闘士伝説！",
+      :channel_name => "テレビ朝日",
+    )
+  end
+
+  describe "#saisoku?" do
+    subject { @program }
+    it { should be_saisoku }
+  end
+end
