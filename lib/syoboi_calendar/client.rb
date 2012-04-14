e7dccff3517646d2b9c3140bcab1070318ce044e
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-14 16:48:03 +0900

    change SyoboiCalendar::Client from class to module

--- lib/syoboi_calendar/client.rb
+++ lib/syoboi_calendar/client.rb
@@ -1,5 +1,5 @@
 class SyoboiCalendar
-  class Client
+  module Client
     # user and pass are optional to search with user's channel setting
     def initialize(opts = {})
       @agent = Agent.new(:user => opts[:user], :pass => opts[:pass])
