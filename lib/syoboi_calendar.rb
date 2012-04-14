417bbb5035ccca724cf05b45d44f7bb4f0ab9e3c
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-14 16:48:33 +0900

    SyoboiCalendar include it's Client

--- lib/syoboi_calendar.rb
+++ lib/syoboi_calendar.rb
@@ -7,3 +7,7 @@ require "syoboi_calendar/agent"
 require "syoboi_calendar/title"
 require "syoboi_calendar/program"
 require "syoboi_calendar/client"
+
+class SyoboiCalendar
+  include SyoboiCalendar::Client
+end
