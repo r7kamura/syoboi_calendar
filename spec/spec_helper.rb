421181f12f3fbbdfee38f5cdfe0bd4bc6f217857
Ryo Nakamura <r7kamura@gmail.com>
2014-04-28 04:20:23 +0900

    Allow net connect to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
 require "simplecov"
 SimpleCov.start
 
+require "webmock/rspec"
+
 if ENV["CI"]
+  WebMock.disable_net_connect!(allow: "codeclimate.com")
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
 end
 
 require "syoboi_calendar"
-require "webmock/rspec"
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
