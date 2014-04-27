5c55dc53c049edf7b95ac565c7d563e12e9f6a6c
Ryo Nakamura <r7kamura@gmail.com>
2014-04-28 00:41:26 +0900

    Sort loaded files in alphavetical order

--- lib/syoboi_calendar.rb
+++ lib/syoboi_calendar.rb
@@ -2,11 +2,11 @@
 require "mechanize"
 
 # libraries
-require "syoboi_calendar/version"
 require "syoboi_calendar/agent"
-require "syoboi_calendar/title"
-require "syoboi_calendar/program"
 require "syoboi_calendar/client"
+require "syoboi_calendar/program"
+require "syoboi_calendar/title"
+require "syoboi_calendar/version"
 
 class SyoboiCalendar
   include SyoboiCalendar::Client
